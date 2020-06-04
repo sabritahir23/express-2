2af269ba9b2391ea8df6dc57ee77fc1ef5c35a94
sabritahir23 <55855625+sabritahir23@users.noreply.github.com>
2020-06-03 17:43:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var data = require('./data/test.json');
 var app = express();
 const PORT = 3000;
 
-//we can now serve up static pages
-app.use(express.static(__dirname));
-
-//app.listen(3000);
+//this is how a route is built in Express
+app.get('/', (req,res)=>{
+res.send(`App is running on port ${PORT}`);
+});
 
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
